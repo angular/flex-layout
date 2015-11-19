3432ad0b2ac4140661704f89bd822f5ac923aece
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-11-19 13:49:51 -0600

    add ngMaterial

--- src/boot.js
+++ src/boot.js
@@ -1,5 +1,10 @@
 
-import angular from 'angular'
+import 'angular-material/angular-material.css!'
+
+// Load Angular libraries
+
+import material from 'angular-material'
+
 import mdMediaQuery from 'mq/MediaQuery.es6'
 import mdLayouts from 'flexbox/Layout.es6'
 
@@ -16,13 +21,23 @@ angular
 
     let body = document.getElementsByTagName("body")[0];
     let app  = angular
-          .module( appName, [ mdMediaQuery, mdLayouts ] )
+          .module( appName, [ mdMediaQuery, mdLayouts, 'ngMaterial'] )
           .controller("TestController", function() {
             let vm = this;
             vm.box1Width = "11";
             vm.direction = "column";
             vm.hideBox = false;
 
+          })
+          .controller("LayoutDemoApp", ($scope) => {
+            $scope.layoutDemo = {
+                mainAxis: 'center',
+                crossAxis: 'center',
+                direction: 'row'
+              };
+              $scope.layoutAlign = function() {
+                return $scope.layoutDemo.mainAxis + ' ' + $scope.layoutDemo.crossAxis;
+              };
           });
 
     angular.bootstrap( body, [ app.name ], { strictDi: false })
