fa47c889f54044005b1c92f3d35590eb3849cb2f
Ryan Schmukler <rschmukler@gmail.com>
2016-01-18 17:18:31 -0500

    build(typescript): add typescript and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var watch = require('gulp-watch');
+
+
+var typescript = require('typescript');
+
+var CONFIG = {
+  srcPath: 'src/**/*.ts',
+  outDir: 'dist/'
+};
+
+// Create a gulp.Typescript project allowing for incremental compilation
+// which means faster builds.
+var tsProject = ts.createProject('tsconfig.json', {
+  typescript: typescript
+});
+
+gulp.task('build-ts', function() {
+  var tsCode = tsProject.src(CONFIG.srcPath)
+    .pipe(ts(tsProject));
+
+  return tsCode.pipe(gulp.dest(CONFIG.outDir));
+});
+
+gulp.task('watch', function() {
+  watch(CONFIG.srcPath, function() {
+    gulp.start('build-ts');
+  });
+});
+
+gulp.task('default', ['watch']);
