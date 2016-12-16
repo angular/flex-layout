f1fa9074d2d91c67023296de3a9bb9e2e7ed2cbc
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-12-16 10:52:14 -0600

    update(demo): add demo for column spans using calc(...) expressions

--- src/demo-app/app/stack-overflow/DemosStackOverflow.ts
+++ src/demo-app/app/stack-overflow/DemosStackOverflow.ts
@@ -5,6 +5,7 @@ import {Component} from '@angular/core';
   template: `
     <demo-complex-column-ordering></demo-complex-column-ordering>
     <demo-grid-area-row-span></demo-grid-area-row-span>
+    <demo-grid-column-span></demo-grid-column-span>
   `
 })
 export class DemosStackOverflow { }
@@ -16,11 +17,13 @@ import {FlexLayoutModule}         from "../../../lib";        // `gulp build:com
 
 import { DemoComplexColumnOrder } from "./columnOrder.demo";
 import {DemoGridAreaRowSpan} from './gridArea.demo';
+import {DemoGridColumnSpan} from './columnSpan.demo';
 
 @NgModule({
   declarations : [
     DemosStackOverflow,     // used by the Router with the root app component
     DemoComplexColumnOrder,
+    DemoGridColumnSpan,
     DemoGridAreaRowSpan
   ],
   imports : [
