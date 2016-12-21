057fd5c6eec57e94b300eb49d53d38b611e25728
Thomas Burleson <ThomasBurleson@Gmail.com>
2016-12-21 13:05:53 -0600

    update(demo): exclude ‘src/lib/utils/testing’ from WebPack builds

--- src/demo-app/webpack.config.js
+++ src/demo-app/webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
       {
         test: /\.ts$/,
         loaders: [ 'awesome-typescript-loader', 'angular2-template-loader' ],
-        exclude: [/\.(spec|e2e)\.ts$/, /node_modules/, /demos/, /deprecated/]
+        exclude: [/\.(spec|e2e)\.ts$/, /node_modules/, /demos/, /deprecated/, /testing/]
         // noParse : [ /angular/, /\@angular/ ]
       },
       {
