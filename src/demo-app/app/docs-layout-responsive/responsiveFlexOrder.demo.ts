aa0ff13c82409da4056cdb8e84e622fb7470c3bd
Bowen Ni <bowen1897@gmail.com>
2017-01-06 11:13:54 -0800

    Fix rxjs import

--- src/demo-app/app/docs-layout-responsive/responsiveFlexOrder.demo.ts
+++ src/demo-app/app/docs-layout-responsive/responsiveFlexOrder.demo.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Inject, OnDestroy} from '@angular/core';
-import {Subscription} from "rxjs";
+import {Subscription} from "rxjs/Subscription";
 import 'rxjs/add/operator/filter';
 
 import {MediaChange} from "../../../lib/media-query/media-change";
