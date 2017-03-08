2340a1921bc25c960faa29de5c697ac776fda18e
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-03-08 13:31:18 -0600

    feat(layout): add wrap options support to fxLayout (#207)
    * feat(fxLayout): add 'wrap' options support to fxLayout
    
    * add support for wrap options as part of fxLayout
    * marked fxLayoutWrap as deprecated (but still supported)
    
    * fix(fxFlex): improve support of flex-basis variations
    
    * improve support for fxFlex options and parsing
    * add calc() auto-corrections for bad whitespacing
    * add basis-validator tools

---
+++ src/lib/utils/basis-validator.ts
@@ -0,0 +1,52 @@
+/**
+ * The flex API permits 3 or 1 parts of the value:
+ *    - `flex-grow flex-shrink flex-basis`, or
+ *    - `flex-basis`
+ * Flex-Basis values can be complicated short-hand versions such as:
+ *   - "3 3 calc(15em + 20px)"
+ *   - "calc(15em + 20px)"
+ *   - "calc(15em+20px)"
+ *   - "37px"
+ *   = "43%"
+ */
+export function validateBasis(basis: string, grow = "1", shrink = "1"): string[] {
+  let parts = [grow, shrink, basis];
+
+  let j = basis.indexOf('calc');
+  if (j > 0) {
+    parts[2] = _validateCalcValue(basis.substring(j).trim());
+    let matches = basis.substr(0, j).trim().split(" ");
+    if (matches.length == 2) {
+      parts[0] = matches[0];
+      parts[1] = matches[1];
+    }
+  } else if (j == 0) {
+    parts[2] = _validateCalcValue(basis.trim());
+  } else {
+    let matches = basis.split(" ");
+    parts = (matches.length === 3) ? matches : [
+      grow, shrink, basis
+    ];
+  }
+
+  return parts;
+}
+
+
+/**
+ * Calc expressions require whitespace before & after the operator
+ * This is a simple, crude whitespace padding solution.
+ */
+function _validateCalcValue(calc: string): string {
+  let operators = ["+", "-", "*", "/"];
+  let findOperator = () => operators.reduce((index, operator) => {
+    return index || (calc.indexOf(operator) + 1);
+  }, 0);
+
+  if (findOperator() > 0) {
+    calc = calc.replace(/[\s]/g, "");
+    let offset = findOperator() - 1;
+    calc = calc.substr(0, offset) + " " + calc.charAt(offset) + " " + calc.substr(offset + 1);
+  }
+  return calc;
+}
