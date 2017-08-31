18cbe253f4a6941f8685c4ff20d8cccb7eecd708
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-08-31 17:10:47 +0000

    8b8b595 chore(build): update known release versions

--- @angular/flex-layout.js
+++ @angular/flex-layout.js
@@ -12,7 +12,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { filter } from 'rxjs/operator/filter';
 import { NgClass, NgStyle } from '@angular/common';
 
-const VERSION = new Version('2.0.0-beta.9-301b563');
+const VERSION = new Version('2.0.0-beta.9-8b8b595');
 
 const LAYOUT_VALUES = ['row', 'column', 'row-reverse', 'column-reverse'];
 function buildLayoutCSS(value) {
