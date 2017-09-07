52eb567ac62267d088bdebb5f135bafc8ebca556
tinayuangao <tinagao@google.com>
2017-09-07 18:16:55 +0000

    6b457dc chore: remove extra space in LICENSE file (#411)

--- @angular/flex-layout.js
+++ @angular/flex-layout.js
@@ -12,7 +12,7 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { filter } from 'rxjs/operator/filter';
 import { NgClass, NgStyle } from '@angular/common';
 
-const VERSION = new Version('2.0.0-beta.9-8b8b595');
+const VERSION = new Version('2.0.0-beta.9-6b457dc');
 
 const LAYOUT_VALUES = ['row', 'column', 'row-reverse', 'column-reverse'];
 function buildLayoutCSS(value) {
