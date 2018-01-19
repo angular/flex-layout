8281049fa7518949cc235b2e94d20c9c51be932a
Adam Plumer <caerus.karu@gmail.com>
2018-01-19 15:14:04 -0500

    build: set ci sudo to false

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -86,7 +86,7 @@ module.exports = (config) => {
     browserDisconnectTimeout: 20000,
     browserNoActivityTimeout: 240000,
     captureTimeout: 120000,
-    browsers: ['Chrome_1024x768'],
+    browsers: ['ChromeHeadlessCISandbox'],
 
     singleRun: false,
 
