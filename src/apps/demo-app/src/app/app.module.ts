9817c63605676d303567d10cfdfa3c0fcbe9ef3a
Adam Plumer <caerus.karu@gmail.com>
2018-03-19 13:06:46 -0400

    feat(demo-app): use/register custom breakpoints
    * Add a custom breakpoint in the demo-app to ensure it works with
      AOT
    * Deprecate the `provideBreakPoints` static method on the main
      module

--- src/apps/demo-app/src/app/app.module.ts
+++ src/apps/demo-app/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlexLayoutModule, BREAKPOINT} from '@angular/flex-layout';
 
 import {RoutingModule} from './routing.module';
 import {AppComponent} from './app.component';
 import {DemoMaterialModule} from './material.module';
 
+const PRINT_BREAKPOINTS = [{
+  alias: 'xs.print',
+  suffix: 'XsPrint',
+  mediaQuery: 'print and (max-width: 297px)',
+  overlapping: false
+}];
 
 @NgModule({
   declarations: [
@@ -19,7 +25,7 @@ import {DemoMaterialModule} from './material.module';
     DemoMaterialModule,
     FlexLayoutModule,
   ],
-  providers: [],
+  providers: [{provide: BREAKPOINT, useValue: PRINT_BREAKPOINTS, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
