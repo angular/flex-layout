6ad68718ad4fae5b7683b18e661ded52b0876ad7
Adam Plumer <caerus.karu@gmail.com>
2018-04-13 20:27:28 -0400

    build: switch release methods

--- tools/gulp/tasks/validate-release.ts
+++ tools/gulp/tasks/validate-release.ts
@@ -3,8 +3,8 @@ import {readFileSync} from 'fs';
 import {join} from 'path';
 import {green, red} from 'chalk';
 import {releasePackages} from './publish';
+import {sync as glob} from 'glob';
 import {buildConfig, sequenceTask} from 'lib-build-tools';
-
 
 /** Path to the directory where all releases are created. */
 const releasesDir = join(buildConfig.outputDir, 'releases');
@@ -37,9 +37,19 @@ task('validate-release:check-bundles', () => {
 
 /** Task that validates the given release package before releasing. */
 function checkReleasePackage(packageName: string): string[] {
-  const bundlePath = join(releasesDir, packageName, '@angular', `${packageName}.js`);
+  return glob(join(releasesDir, packageName, 'esm2015/*.js'))
+    .reduce((failures: string[], bundlePath: string) => {
+      return failures.concat(checkEs2015ReleaseBundle(bundlePath));
+    }, []);
+}
+
+/**
+ * Checks an ES2015 bundle inside of a release package. Secondary entry-point bundles will be
+ * checked as well.
+ */
+function checkEs2015ReleaseBundle(bundlePath: string): string[] {
   const bundleContent = readFileSync(bundlePath, 'utf8');
-  let failures = [];
+  let failures: string[] = [];
 
   if (inlineStylesSourcemapRegex.exec(bundleContent) !== null) {
     failures.push('Bundles contain sourcemap references in component styles.');
@@ -51,6 +61,4 @@ function checkReleasePackage(packageName: string): string[] {
 
   return failures;
 }
-
-
 
