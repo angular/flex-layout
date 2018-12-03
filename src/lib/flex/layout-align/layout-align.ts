defc3dfb17f6dcb4ce29be47510fcf65e4f5c2b3
Vojtech Mašek <vojtech.masek@flowup.cz>
2018-12-03 20:35:51 +0100

    feat(fxLayoutAlign): value types

--- src/lib/flex/layout-align/layout-align.ts
+++ src/lib/flex/layout-align/layout-align.ts
@@ -31,6 +31,88 @@ import {extendObject} from '../../utils/object-extend';
 import {Layout, LayoutDirective} from '../layout/layout';
 import {LAYOUT_VALUES, isFlowHorizontal} from '../../utils/layout-validator';
 
+type LayoutAlignMainAxisValuesType
+  = ''
+  | 'start' | 'flex-start'
+  | 'flex-start'
+  | 'center'
+  | 'end' | 'flex-end'
+  | 'space-around'
+  | 'space-between'
+  | 'space-evenly'
+  ;
+
+type LayoutAlignCrossAxisValuesType
+  = LayoutAlignMainAxisValuesType
+  | 'stretch'
+  | 'baseline'
+  ;
+
+/**
+ * Combinations of all flex layout align values
+ *
+ * @see https://github.com/angular/flex-layout/wiki/fxLayoutAlign-API
+ */
+type LayoutAlignValuesType
+  = LayoutAlignMainAxisValuesType
+// Main Axis combinations with Cross Axis - start (flex-start)
+  | 'start start' | 'start flex-start'
+  | 'center start' | 'center flex-start'
+  | 'end start' | 'end flex-start'
+  | 'space-around start' | 'space-around flex-start'
+  | 'space-between start' | 'space-between flex-start'
+  | 'space-evenly start' | 'space-evenly flex-start'
+// Main Axis combinations with Cross Axis - center
+  | 'start center'
+  | 'center center'
+  | 'end center'
+  | 'space-around center'
+  | 'space-between center'
+  | 'space-evenly center'
+// Main Axis combinations with Cross Axis - end (flex-end)
+  | 'start end' | 'start flex-end'
+  | 'center end' | 'center flex-end'
+  | 'end end' | 'end flex-end'
+  | 'space-around end' | 'space-around flex-end'
+  | 'space-between end' | 'space-between flex-end'
+  | 'space-evenly end' | 'space-evenly flex-end'
+// Main Axis combinations with Cross Axis - space-around
+  | 'start space-around'
+  | 'center space-around'
+  | 'end space-around'
+  | 'space-around space-around'
+  | 'space-between space-around'
+  | 'space-evenly space-around'
+// Main Axis combinations with Cross Axis - space-between
+  | 'start space-between'
+  | 'center space-between'
+  | 'end space-between'
+  | 'space-around space-between'
+  | 'space-between space-between'
+  | 'space-evenly space-between'
+// Main Axis combinations with Cross Axis - space-evenly
+  | 'start space-evenly'
+  | 'center space-evenly'
+  | 'end space-evenly'
+  | 'space-around space-evenly'
+  | 'space-between space-evenly'
+  | 'space-evenly space-evenly'
+// Main Axis combinations with Cross Axis - stretch
+  | 'start stretch'
+  | 'center stretch'
+  | 'end stretch'
+  | 'space-around stretch'
+  | 'space-between stretch'
+  | 'space-evenly stretch'
+// Main Axis combinations with Cross Axis - baseline
+  | 'start baseline'
+  | 'center baseline'
+  | 'end baseline'
+  | 'space-around baseline'
+  | 'space-between baseline'
+  | 'space-evenly baseline'
+  ;
+
 export interface LayoutAlignParent {
   layout: string;
 }
@@ -38,7 +120,8 @@ export interface LayoutAlignParent {
 @Injectable({providedIn: 'root'})
 export class LayoutAlignStyleBuilder extends StyleBuilder {
   buildStyles(align: string, parent: LayoutAlignParent) {
-    const css: StyleDefinition = {}, [mainAxis, crossAxis] = align.split(' ');
+    const css: StyleDefinition = {}, [mainAxis, crossAxis] =
+        align.split(' ') as [LayoutAlignMainAxisValuesType, LayoutAlignCrossAxisValuesType];
 
     // Main axis
     switch (mainAxis) {
@@ -129,22 +212,22 @@ export class LayoutAlignDirective extends BaseDirective implements OnInit, OnCha
   protected _layoutWatcher?: Subscription;
 
   /* tslint:disable */
-  @Input('fxLayoutAlign')       set align(val: string)     { this._cacheInput('align', val); }
-  @Input('fxLayoutAlign.xs')    set alignXs(val: string)   { this._cacheInput('alignXs', val); }
-  @Input('fxLayoutAlign.sm')    set alignSm(val: string)   { this._cacheInput('alignSm', val); };
-  @Input('fxLayoutAlign.md')    set alignMd(val: string)   { this._cacheInput('alignMd', val); };
-  @Input('fxLayoutAlign.lg')    set alignLg(val: string)   { this._cacheInput('alignLg', val); };
-  @Input('fxLayoutAlign.xl')    set alignXl(val: string)   { this._cacheInput('alignXl', val); };
+  @Input('fxLayoutAlign')       set align(val: LayoutAlignValuesType)     { this._cacheInput('align', val); }
+  @Input('fxLayoutAlign.xs')    set alignXs(val: LayoutAlignValuesType)   { this._cacheInput('alignXs', val); }
+  @Input('fxLayoutAlign.sm')    set alignSm(val: LayoutAlignValuesType)   { this._cacheInput('alignSm', val); };
+  @Input('fxLayoutAlign.md')    set alignMd(val: LayoutAlignValuesType)   { this._cacheInput('alignMd', val); };
+  @Input('fxLayoutAlign.lg')    set alignLg(val: LayoutAlignValuesType)   { this._cacheInput('alignLg', val); };
+  @Input('fxLayoutAlign.xl')    set alignXl(val: LayoutAlignValuesType)   { this._cacheInput('alignXl', val); };
 
-  @Input('fxLayoutAlign.gt-xs') set alignGtXs(val: string) { this._cacheInput('alignGtXs', val); };
-  @Input('fxLayoutAlign.gt-sm') set alignGtSm(val: string) { this._cacheInput('alignGtSm', val); };
-  @Input('fxLayoutAlign.gt-md') set alignGtMd(val: string) { this._cacheInput('alignGtMd', val); };
-  @Input('fxLayoutAlign.gt-lg') set alignGtLg(val: string) { this._cacheInput('alignGtLg', val); };
+  @Input('fxLayoutAlign.gt-xs') set alignGtXs(val: LayoutAlignValuesType) { this._cacheInput('alignGtXs', val); };
+  @Input('fxLayoutAlign.gt-sm') set alignGtSm(val: LayoutAlignValuesType) { this._cacheInput('alignGtSm', val); };
+  @Input('fxLayoutAlign.gt-md') set alignGtMd(val: LayoutAlignValuesType) { this._cacheInput('alignGtMd', val); };
+  @Input('fxLayoutAlign.gt-lg') set alignGtLg(val: LayoutAlignValuesType) { this._cacheInput('alignGtLg', val); };
 
-  @Input('fxLayoutAlign.lt-sm') set alignLtSm(val: string) { this._cacheInput('alignLtSm', val); };
-  @Input('fxLayoutAlign.lt-md') set alignLtMd(val: string) { this._cacheInput('alignLtMd', val); };
-  @Input('fxLayoutAlign.lt-lg') set alignLtLg(val: string) { this._cacheInput('alignLtLg', val); };
-  @Input('fxLayoutAlign.lt-xl') set alignLtXl(val: string) { this._cacheInput('alignLtXl', val); };
+  @Input('fxLayoutAlign.lt-sm') set alignLtSm(val: LayoutAlignValuesType) { this._cacheInput('alignLtSm', val); };
+  @Input('fxLayoutAlign.lt-md') set alignLtMd(val: LayoutAlignValuesType) { this._cacheInput('alignLtMd', val); };
+  @Input('fxLayoutAlign.lt-lg') set alignLtLg(val: LayoutAlignValuesType) { this._cacheInput('alignLtLg', val); };
+  @Input('fxLayoutAlign.lt-xl') set alignLtXl(val: LayoutAlignValuesType) { this._cacheInput('alignLtXl', val); };
 
   /* tslint:enable */
   constructor(
