854cfa2ed2f19058647ea6df24ecb8c7c3a696dd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-01-10 18:26:39 +0100

    Fixes that the server entry-point is re-exported at the root

--- tools/gulp/packages.ts
+++ tools/gulp/packages.ts
@@ -5,4 +5,8 @@ export const flexLayoutPackage = new BuildPackage('flex-layout', []);
 
 // To avoid refactoring of the project the Flex-Layout package will map to the source path `lib/`.
 flexLayoutPackage.sourceDir = join(buildConfig.packagesDir, 'lib');
+
+// Re-export secondary entry-points at the root. We don't want to require users to add
+// UMD bundles for each secondary entry-points.
 flexLayoutPackage.exportsSecondaryEntryPointsAtRoot = true;
+flexLayoutPackage.exportsSecondaryEntryPointsAtRootExcludes = ['server'];
