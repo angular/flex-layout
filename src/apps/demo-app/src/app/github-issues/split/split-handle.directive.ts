1355a0f90f025a82d32fba0bf9188ea6ae24f676
Adam Plumer <caerus.karu@gmail.com>
2019-01-13 12:14:35 -0600

    fixup! docs: make splitter work in IE

--- src/apps/demo-app/src/app/github-issues/split/split-handle.directive.ts
+++ src/apps/demo-app/src/app/github-issues/split/split-handle.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, ElementRef, Inject, Output} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {fromEvent, Observable} from 'rxjs';
-import {map, switchMap, takeUntil, tap} from 'rxjs/operators';
+import {map, switchMap, takeUntil, throttleTime} from 'rxjs/operators';
 
 @Directive({
   selector: '[ngxSplitHandle]',
@@ -14,24 +14,17 @@ export class SplitHandleDirective {
   @Output() drag: Observable<{ x: number, y: number }>;
 
   constructor(ref: ElementRef, @Inject(DOCUMENT) _document: any) {
-    let prevX = 0;
-    let prevY = 0;
-    const getMouseEventPosition = (event: MouseEvent) => {
-      const movement = {x: event.screenX - prevX, y: event.screenY - prevY};
-      prevX = event.screenX;
-      prevY = event.screenY;
-      return movement;
+    let last = {x: 0, y: 0};
+    const scanPositionDelta = (event: MouseEvent) => {
+      const current = {x: event.screenX, y: event.screenY};
+      const delta = {x: current.x - last.x, y: current.y - last.y};
+      last = current;
+      return delta;
     };
 
-    const mousedown$ = fromEvent(ref.nativeElement, 'mousedown').pipe(map(getMouseEventPosition));
-    const mousemove$ = fromEvent(_document, 'mousemove').pipe(map(getMouseEventPosition));
-    const mouseup$ = fromEvent(_document, 'mouseup').pipe(
-      tap(() => {
-        prevX = 0;
-        prevY = 0;
-      }),
-      map(getMouseEventPosition)
-    );
+    const mousedown$ = fromEvent(ref.nativeElement, 'mousedown').pipe(map(scanPositionDelta));
+    const mousemove$ = fromEvent(window, 'mousemove').pipe(throttleTime(50), map(scanPositionDelta));
+    const mouseup$ = fromEvent(window, 'mouseup');
 
     this.drag = mousedown$.pipe(switchMap(() => mousemove$.pipe(takeUntil(mouseup$))));
   }
