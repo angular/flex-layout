ec50d54e1fa0e1a46c619acb8263bbac0ffae1aa
Adam Plumer <caerus.karu@gmail.com>
2019-01-13 00:08:35 -0600

    docs: make splitter work in IE

--- src/apps/demo-app/src/app/github-issues/split/split-handle.directive.ts
+++ src/apps/demo-app/src/app/github-issues/split/split-handle.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, ElementRef, Inject, Output} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {fromEvent, Observable} from 'rxjs';
-import {map, switchMap, takeUntil} from 'rxjs/operators';
+import {map, switchMap, takeUntil, tap} from 'rxjs/operators';
 
 @Directive({
   selector: '[ngxSplitHandle]',
@@ -14,11 +14,24 @@ export class SplitHandleDirective {
   @Output() drag: Observable<{ x: number, y: number }>;
 
   constructor(ref: ElementRef, @Inject(DOCUMENT) _document: any) {
-    const getMouseEventPosition = (event: MouseEvent) => ({x: event.movementX, y: event.movementY});
+    let prevX = 0;
+    let prevY = 0;
+    const getMouseEventPosition = (event: MouseEvent) => {
+      const movement = {x: event.screenX - prevX, y: event.screenY - prevY};
+      prevX = event.screenX;
+      prevY = event.screenY;
+      return movement;
+    };
 
     const mousedown$ = fromEvent(ref.nativeElement, 'mousedown').pipe(map(getMouseEventPosition));
     const mousemove$ = fromEvent(_document, 'mousemove').pipe(map(getMouseEventPosition));
-    const mouseup$ = fromEvent(_document, 'mouseup').pipe(map(getMouseEventPosition));
+    const mouseup$ = fromEvent(_document, 'mouseup').pipe(
+      tap(() => {
+        prevX = 0;
+        prevY = 0;
+      }),
+      map(getMouseEventPosition)
+    );
 
     this.drag = mousedown$.pipe(switchMap(() => mousemove$.pipe(takeUntil(mouseup$))));
   }
