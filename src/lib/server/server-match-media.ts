e8ded3e0f664e8cd02262b9970318f552bfc351b
CaerusKaru <caerus.karu@gmail.com>
2020-05-07 19:29:56 -0500

    build: upgrade to Angular v9.1, CDK v9.2, and TypeScript 3.8 (#1240)

--- src/lib/server/server-match-media.ts
+++ src/lib/server/server-match-media.ts
@@ -48,7 +48,7 @@ export class ServerMediaQueryList implements MediaQueryList {
       this._isActive = true;
       this._listeners.forEach((callback) => {
         const cb: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) = callback!;
-        cb.call(null, this);
+        cb.call(this, {matches: this.matches, media: this.media} as MediaQueryListEvent);
       });
     }
     return this;
@@ -60,7 +60,7 @@ export class ServerMediaQueryList implements MediaQueryList {
       this._isActive = false;
       this._listeners.forEach((callback) => {
         const cb: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) = callback!;
-        cb.call(null, this);
+        cb.call(this, {matches: this.matches, media: this.media} as MediaQueryListEvent);
       });
     }
     return this;
@@ -73,7 +73,7 @@ export class ServerMediaQueryList implements MediaQueryList {
     }
     if (this._isActive) {
       const cb: ((this: MediaQueryList, ev: MediaQueryListEvent) => any) = listener!;
-      cb.call(null, this);
+      cb.call(this, {matches: this.matches, media: this.media} as MediaQueryListEvent);
     }
   }
 
