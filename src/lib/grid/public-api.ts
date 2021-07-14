2c1b959ac98efd5315e783fdfb6f58f84af05009
ruslic19 <48139997+ruslic19@users.noreply.github.com>
2021-07-14 13:27:22 +0300

    build: export grid directives (#1353)
    Previously only the top-level module was exported as part of the public API. This posed a problem when trying to extend the directives with custom breakpoints.

--- src/lib/grid/public-api.ts
+++ src/lib/grid/public-api.ts
@@ -8,4 +8,14 @@
 
 export * from './module';
 
-
+export * from './align-columns/align-columns';
+export * from './align-rows/align-rows';
+export * from './area/area';
+export * from './areas/areas';
+export * from './auto/auto';
+export * from './column/column';
+export * from './columns/columns';
+export * from './gap/gap';
+export * from './grid-align/grid-align';
+export * from './row/row';
+export * from './rows/rows';
